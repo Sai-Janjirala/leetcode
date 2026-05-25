48db0c2c4d38df50b10977c8b5a2189fee754f2d
Sai Janjirala <saijanjirala5411@gmail.com>
2026-05-26 02:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (98.21%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSegments(string s) {
+
+        int count = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            // Word ending found
+            if(s[i] != ' ' &&
+              (i == s.length() - 1 || s[i + 1] == ' ')) {
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
