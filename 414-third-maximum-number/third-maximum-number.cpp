dfd843a899c4f4a1929df1ea5cbb15f378c3b126
Sai Janjirala <saijanjirala5411@gmail.com>
2026-06-07 18:19:46 +0530

    Time: 6 ms (12.75%) | Memory: 14.8 MB (5.31%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-
         set<int> num;
-
-        // Store unique elements
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 0 ; i < nums.size() ; i++){
             num.insert(nums[i]);
         }
-
-        // Convert set into vector
         vector<int> ans;
-
-        for(int x : num) {
+        for( int x: num){
             ans.push_back(x);
         }
-
-        // If less than 3 distinct elements
-        if(ans.size() < 3) {
-            return ans[ans.size() - 1];
+        if(ans.size() < 3){
+            return ans[ans.size() -1];
         }
 
-        // Return 3rd largest
-        return ans[ans.size() - 3];
+        return ans[ans.size() -3];
+
     }
 };
