e19be4a105e458fae75ae70bd7ec79a33b946b24
Sai Janjirala <saijanjirala5411@gmail.com>
2026-06-07 18:41:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (81.96%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int countSegments(string s) {
-
-        int count = 0;
-
-        for(int i = 0; i < s.length(); i++) {
-
-            // Word ending found
-            if(s[i] != ' ' &&
-              (i == s.length() - 1 || s[i + 1] == ' ')) {
-
+        int count = 0 ;
+         for(int i = 0 ; i < s.length(); i++){
+            if((s[i] != ' ') &&(i == s.length() - 1 || s[i+1] == ' ') ){
                 count++;
             }
-        }
-
-        return count;
+         }
+         return count;
     }
 };
