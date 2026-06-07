7cd39f2695f044498e1c75e5eb62cecea5076087
Sai Janjirala <saijanjirala5411@gmail.com>
2026-06-07 17:38:24 +0530

    Time: 1 ms (19.97%) | Memory: 9.3 MB (25.00%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
-        for(int i = 0; i < s.length(); i++) {
-
-            if(s[i] != t[i]) {
-
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s[i] != t[i]){
                 return t[i];
             }
         }
 
-        // Extra character at end
-        return t[t.length() - 1];
+        return t[t.length() -1];
     }
 };
