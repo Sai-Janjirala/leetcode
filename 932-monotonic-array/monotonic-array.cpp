df783c465426329450a54fe2cf1c2968a10749d1
Sai Janjirala <saijanjirala5411@gmail.com>
2026-07-23 13:52:31 +0530

    Time: 0 ms (100.00%) | Memory: 100.3 MB (69.82%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -5,17 +5,12 @@ public:
         bool increasing = true;
         bool decreasing = true;
 
-        for(int i = 0; i < nums.size() - 1; i++) {
-
-            if(nums[i] < nums[i + 1]) {
-                decreasing = false;
-            }
-
-            if(nums[i] > nums[i + 1]) {
+        for(int i = 0; i < nums.size()-1; i++) {
+            if(nums[i] > nums[i+1])
                 increasing = false;
-            }
+            if(nums[i] < nums[i+1])
+                decreasing = false;
         }
-
         return increasing || decreasing;
     }
 };
