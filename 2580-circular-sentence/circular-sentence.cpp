12f39bfa4646eff94a3bb6a3041089c3b0b8cf07
Sai Janjirala <saijanjirala5411@gmail.com>
2026-07-29 13:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (51.05%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        if (sentence.front() != sentence.back())
+            return false;
+
+        for (int i = 0; i < sentence.size(); i++) {
+            if (sentence[i] == ' ') {
+                if (sentence[i - 1] != sentence[i + 1])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
