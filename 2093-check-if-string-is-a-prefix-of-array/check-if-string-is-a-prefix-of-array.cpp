226b29b672cf471bed2aee40dcc1a327f7de7a0f
Sai Janjirala <saijanjirala5411@gmail.com>
2026-08-03 09:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (35.50%) - LeetSync

---
+++ 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        string temp = "";
+
+        for (string word : words) {
+            temp += word;
+
+            if (temp == s)
+                return true;
+
+            if (temp.size() > s.size())
+                return false;
+        }
+
+        return false;
+    }
+};
